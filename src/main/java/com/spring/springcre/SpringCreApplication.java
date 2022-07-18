8e1fba4cea95a13d8dcec9c6e9a4b3bc255db9e8
황두리 <hdr@gabia.com>
2022-07-18 11:07:40 +0900

    first commit

---
+++ src/main/java/com/spring/springcre/SpringCreApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springcre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/springcre/SpringCreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springcre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
